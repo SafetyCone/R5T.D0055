cf2c2c6bc8d360d5659220febebbf2c9ac0e16ac
David Coats <david@getsafetycone.com>
2021-04-30 16:02:24 -0700

    Add R5T.D0055.Base service definitions library project. Remove extraneous R5T.D0055 library.

---
+++ source/R5T.D0055.Base/Code/Services/Definitions/IEmailOutputDirectoryPathProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace R5T.D0055
+{
+    /// <summary>
+    /// Provides a stringly-typed directory path to which emails can be saved as files.
+    /// </summary>
+    public interface IEmailOutputDirectoryPathProvider
+    {
+        Task<string> GetEmailOutputDirectoryPath();
+    }
+}
