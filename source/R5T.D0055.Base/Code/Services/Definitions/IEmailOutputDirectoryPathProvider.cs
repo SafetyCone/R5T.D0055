3893dc789802ccfde6723421be118fe4889e7fcf
davidcoats <david@getsafetycone.com>
2022-04-27 15:27:45 -0700

    Interim changes.

--- source/R5T.D0055.Base/Code/Services/Definitions/IEmailOutputDirectoryPathProvider.cs
+++ source/R5T.D0055.Base/Code/Services/Definitions/IEmailOutputDirectoryPathProvider.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Threading.Tasks;
 
+using R5T.T0064;
+
 
 namespace R5T.D0055
 {
     /// <summary>
     /// Provides a stringly-typed directory path to which emails can be saved as files.
     /// </summary>
-    public interface IEmailOutputDirectoryPathProvider
+    [ServiceDefinitionMarker]
+    public interface IEmailOutputDirectoryPathProvider : IServiceDefinition
     {
         Task<string> GetEmailOutputDirectoryPath();
     }
